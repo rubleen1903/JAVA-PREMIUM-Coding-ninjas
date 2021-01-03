9d1c492e9ff1836fab28f2f78d08588b9f3a7be1
rubleen1903 <rubleen16@gmail.com>
2021-01-04 01:56:12 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {    //println is used to print the output in the next line.
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.print("By Rubleen");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main (String [] args)
+    {  Scanner sc= new Scanner(System.in);
+        int i= sc.nextInt();
+        int j=sc.nextInt();
+        int k=i+j;
+        System.out.println(k);
+        sc.close();
+    }
+}
